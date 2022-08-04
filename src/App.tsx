c3d44f1414a1f8ec7f029a06b6bfdb1a3bb1064c
Shakirul Hasan <shakirulhkhan@gmail.com>
2022-08-04 12:53:30 +0600

    init: scaffold a react project and remove unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Box, Heading } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Heading size="2xl" textAlign="center" color="primary.red">
+        Hello STT-TTS
+      </Heading>
+    </Box>
   );
 }
 
---
+++ src/config/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  primary: {
+    red: "#EF476F",
+    blue: "#118AB2",
+    green: "#06D6A0",
+    yellow: "#FFD166",
+    dark: "#073B4C",
+  },
+};
+
+const theme = extendTheme({ colors });
+
+export default theme;
