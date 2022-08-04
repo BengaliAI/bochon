f952e56bfbebf8b32dce9e50e84f6e061a8d2a82
Shakirul Hasan <shakirulhkhan@gmail.com>
2022-08-05 01:50:35 +0600

    r&d: live speech-to-text using azure's speech sdk

--- src/features/stt.tsx
+++ src/features/stt.tsx
@@ -1,10 +1,22 @@
 import { Button, Center, Heading, Stack, Textarea } from "@chakra-ui/react";
 import { useState } from "react";
 import RecordRTC from "recordrtc";
+import * as sdk from "microsoft-cognitiveservices-speech-sdk";
+
+const speechConfig = sdk.SpeechConfig.fromSubscription(
+  process.env.REACT_APP_AZURE_SECRET_KEY || "",
+  process.env.REACT_APP_AZURE_REGION || ""
+);
+speechConfig.speechRecognitionLanguage = "en-US";
 
 export const STT = () => {
+  const [recognizedText, setRecognizedText] = useState("");
+  const [recognizingText, setRecognizingText] = useState("");
   const [recordedAudio, setRecordedAudio] = useState<Blob | null>(null);
   const [recorder, setRecorder] = useState<RecordRTC | null>(null);
+  const [recognizer, setRecognizer] = useState<sdk.SpeechRecognizer | null>(
+    null
+  );
   const [isRecording, setIsRecording] = useState(false);
 
   const startRecording = async () => {
@@ -12,15 +24,39 @@ export const STT = () => {
     const audioStream = await navigator.mediaDevices.getUserMedia({
       audio: true,
     });
+
+    const audioConfig = sdk.AudioConfig.fromStreamInput(audioStream);
+    const recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+    recognizer.startContinuousRecognitionAsync();
+
+    recognizer.recognized = (s, e) => {
+      console.log("recognized", e);
+      if (e.result.text) {
+        setRecognizingText("");
+        setRecognizedText((prev) => prev + " " + e.result.text);
+      }
+    };
+
+    recognizer.recognizing = (s, e) => {
+      console.log("recognizing", e);
+      if (e.result.text) {
+        setRecognizingText(e.result.text);
+      }
+    };
+
     const audioRecorder = new RecordRTC(audioStream, {
       type: "audio",
     });
     audioRecorder.startRecording();
     setIsRecording(true);
+    setRecognizer(recognizer);
     setRecorder(audioRecorder);
   };
 
   const stopRecording = async () => {
+    if (recognizer) {
+      recognizer.stopContinuousRecognitionAsync();
+    }
     if (recorder) {
       recorder.stopRecording(() => {
         const audioBlob = recorder.getBlob();
@@ -35,7 +71,15 @@ export const STT = () => {
       <Heading size="xl" mb={3}>
         STT
       </Heading>
-      <Textarea placeholder="Text will appear here..." />
+      <Textarea
+        placeholder="Text will appear here..."
+        value={
+          recognizedText || recognizingText
+            ? recognizedText + " " + recognizingText
+            : ""
+        }
+        readOnly
+      />
       <Stack direction="row" spacing={4} mt={3}>
         <Button
           colorScheme="green"
@@ -43,7 +87,6 @@ export const STT = () => {
           size="lg"
           onClick={startRecording}
           isLoading={isRecording}
-          //   spinner={<Bars color="white" />}
         >
           Start
         </Button>
@@ -58,16 +101,6 @@ export const STT = () => {
           </Button>
         )}
 
-        {recordedAudio && (
-          <Button
-            colorScheme="blue"
-            variant="solid"
-            size="lg"
-            onClick={() => console.log(recordedAudio)}
-          >
-            Download
-          </Button>
-        )}
         {recordedAudio && (
           <audio controls src={URL.createObjectURL(recordedAudio)}></audio>
         )}
