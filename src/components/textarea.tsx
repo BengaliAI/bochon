3c6af17967eb00941eb35aa2b4738ed5f70e37c9
Shakirul Hasan <shakirulhkhan@gmail.com>
2022-08-08 11:02:30 +0600

    refactor: text area container code refactored

---
+++ src/components/textarea.tsx
@@ -0,0 +1,52 @@
+import { Flex, Icon, Select } from "@chakra-ui/react";
+import { PropsWithChildren } from "react";
+import { IconType } from "react-icons";
+
+type TextAreaContainerProps = PropsWithChildren<{
+  icon: IconType;
+  title: string;
+  models: { name: string; value: string }[];
+}>;
+
+export const TextAreaContainer = ({
+  children,
+  icon,
+  title,
+  models,
+}: TextAreaContainerProps) => {
+  return (
+    <Flex
+      direction="column"
+      bg="#fff5"
+      borderRadius="xl"
+      shadow="xl"
+      height="100%"
+    >
+      <Flex
+        py={3}
+        px={5}
+        justifyContent="space-between"
+        direction={["column", "column", "row"]}
+        alignItems="center"
+        borderBottom="1px solid #ddd"
+        flexShrink={1}
+      >
+        <Flex fontSize="xl" alignItems="center">
+          <Icon as={icon} me={3} fontSize="2xl" />
+          <span>{title}</span>
+        </Flex>
+        <Select
+          placeholder="Select Model"
+          width={["100%", "100%", "50%"]}
+          mt={[5, 5, 0]}
+          defaultValue="azure"
+        >
+          {models.map((model) => (
+            <option value={model.value}>{model.name}</option>
+          ))}
+        </Select>
+      </Flex>
+      {children}
+    </Flex>
+  );
+};
--- src/components/textareaSTT.tsx
+++ src/components/textareaSTT.tsx
@@ -1,55 +1,21 @@
-import { Select, Flex, Icon, Box, Text } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import { RiMic2Line } from "react-icons/ri";
+import { TextAreaContainer } from "./textarea";
 
 type TextAreaSTTProps = {
   recognizedText: string;
   recognizingText: string;
 };
 
+const models = [{ name: "Microsoft Azure", value: "azure" }];
+
 export const TextAreaSTT = ({
   recognizedText,
   recognizingText,
 }: TextAreaSTTProps) => {
   return (
-    <Flex
-      direction="column"
-      bg="#fff5"
-      borderRadius="xl"
-      shadow="xl"
-      height="100%"
-    >
-      <Flex
-        py={3}
-        px={5}
-        justifyContent="space-between"
-        direction={["column", "column", "row"]}
-        alignItems="center"
-        borderBottom="1px solid #ddd"
-        flexShrink={1}
-      >
-        <Flex fontSize="xl" alignItems="center">
-          <Icon as={RiMic2Line} me={3} fontSize="2xl" />
-          <span>Speech To Text</span>
-        </Flex>
-        <Select
-          placeholder="Select Model"
-          width={["100%", "100%", "50%"]}
-          mt={[5, 5, 0]}
-        >
-          <option value="azure">Microsoft Azure</option>
-          <option value="gcp">Google Cloud</option>
-          <option value="aws">Amazon AWS</option>
-        </Select>
-      </Flex>
-      <Box
-        fontSize="lg"
-        bg="transparent"
-        border="none"
-        height="100%"
-        flexGrow={1}
-        px={10}
-        py={5}
-      >
+    <TextAreaContainer icon={RiMic2Line} title="Speech to Text" models={models}>
+      <Box fontSize="lg" height="100%" flexGrow={1} px={10} py={5}>
         {recognizedText && <Text display="inline">{recognizedText} </Text>}
         {recognizingText && (
           <Text display="inline" color="gray.500">
@@ -57,6 +23,6 @@ export const TextAreaSTT = ({
           </Text>
         )}
       </Box>
-    </Flex>
+    </TextAreaContainer>
   );
 };
