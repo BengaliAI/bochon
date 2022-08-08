a4e34aa7900c6c8c99e8b1536c27d3f1ec58a8cf
Shakirul Hasan <shakirulhkhan@gmail.com>
2022-08-08 10:54:22 +0600

    refactor: toolbox container and items refactor

---
+++ src/components/toolbox.tsx
@@ -0,0 +1,43 @@
+import { Center, CenterProps, Flex, Icon } from "@chakra-ui/react";
+import { PropsWithChildren } from "react";
+import { IconType } from "react-icons";
+
+export const ToolBoxContainer = ({ children }: PropsWithChildren) => {
+  return (
+    <Flex
+      borderRadius="xl"
+      fontSize={["md", "lg", "xl"]}
+      overflow="hidden"
+      shadow="sm"
+      sx={{
+        "&>div": {
+          py: [3, 3, 5],
+          px: [5, 5, 10],
+          borderRight: "1px solid #ddd",
+          cursor: "pointer",
+          transition: "background 0.3s ease",
+          _hover: { background: "#fff8" },
+        },
+        "&>div:not(.record-button)": {
+          background: "white",
+          _hover: { background: "#fff8" },
+        },
+        "&>div:last-child": {
+          border: "none",
+        },
+      }}
+    >
+      {children}
+    </Flex>
+  );
+};
+
+type ToolBoxItemProps = CenterProps & { icon: IconType; title: string };
+
+export const ToolBoxItem = ({ icon, title, ...rest }: ToolBoxItemProps) => {
+  return (
+    <Center title={title} {...rest}>
+      <Icon as={icon} />
+    </Center>
+  );
+};
