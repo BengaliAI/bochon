a11cc9fbe6b00163c0bac330852e8d0e7309d445
Shakirul Hasan <shakirulhkhan@gmail.com>
2022-08-10 02:14:56 +0600

    fix: nav drawer reopen issue fix after path change

--- src/components/navDrawer.tsx
+++ src/components/navDrawer.tsx
@@ -9,9 +9,15 @@ import {
 } from "@chakra-ui/react";
 import { SideNav } from "./sideNav";
 import { RiMenuLine } from "react-icons/ri";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 export const NavDrawer = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { pathname } = useLocation();
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(onClose, [pathname]);
 
   return (
     <>
